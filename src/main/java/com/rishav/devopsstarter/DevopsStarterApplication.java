854eec9b6f54e64f73e1e9a6f39354c1f4696fc9
RishavRajSingh11 <2023sl70004@wilp.bits-pilani.ac.in>
2026-09-25 17:01:58 +0530

    feat: add src/main/java/com/rishav/devopsstarter/DevopsStarterApplication.java

---
+++ src/main/java/com/rishav/devopsstarter/DevopsStarterApplication.java
@@ -0,0 +1,11 @@
+package com.rishav.devopsstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsStarterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsStarterApplication.class, args);
+    }
+}
